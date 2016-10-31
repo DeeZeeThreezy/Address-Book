2ccef96f2cc021c25b5c05ddf574933953e780cd
DeeZeeThreezy <dperez3iii@gmail.com>
2016-10-30 21:24:44 -0700

    Converted working React to webpack

---
+++ Address Book/webpack.config.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+    entry: './Scripts/app/index.js',
+    output: {
+        path: './Scripts',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
